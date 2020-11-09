a94a3f9ac3d2c69fafbba2d467c078404fbdad38
sho <sskmy1024.y@gmail.com>
2020-11-09 23:33:27 +0900

    fix message-log test case

--- test/e2e/message-log.e2e-spec.ts
+++ test/e2e/message-log.e2e-spec.ts
@@ -4,11 +4,44 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { ValidationPipe } from '@nestjs/common';
 
 import { MessageLogModule } from '../../src/modules/message-log/message-log.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { MessageLog } from '../../src/entities/messageLog.entity';
+import { MessageLogDto } from 'src/modules/message-log/message-log.dto';
 
 describe('MessageLogController (e2e)', () => {
   let app: NestFastifyApplication;
+
+  const messageLogs = [
+    {
+      uuid: "3e2b85a1-21cd-4b10-ae5b-abe8fe09305d",
+      sender: 'user',
+      message: 'hello world!',
+      createdAt: new Date(2020, 10, 10, 9, 50)
+    }
+  ]
+
+  const MockRepository = {
+    provide: getRepositoryToken(MessageLog),
+    useValue: {
+      find: () => messageLogs,
+      insert: entity => ({
+        "identifiers": [
+            {
+                "uuid": entity.uuid
+            }
+        ],
+        "generatedMaps": [
+            {
+                "uuid": entity.uuid,
+                "createdAt": entity.createdAt
+            }
+        ],
+        "raw": [...messageLogs, entity].length
+      }),
+      update: (id, entity) => entity,
+      delete: () => messageLogs.splice(0, 1),
+    },
+  }
 
   beforeAll(async () => {
     process.env.UIX_BASE_PATH = path.resolve(__dirname, '../../');
@@ -17,7 +50,7 @@ describe('MessageLogController (e2e)', () => {
 
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([MessageLog]), MessageLogModule]
-    }).compile();
+    }).overrideProvider(MockRepository.provide).useValue(MockRepository.useValue).compile();
 
     app = moduleFixture.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
 
@@ -37,17 +70,19 @@ describe('MessageLogController (e2e)', () => {
     });
 
     expect(res.statusCode).toEqual(200);
-    expect(res.json()).toEqual([]);
+    expect(res.json()).toEqual(JSON.parse(JSON.stringify(messageLogs)));
   });
 
   it('POST /message-log (valid send message)', async () => {
+    const payload : MessageLogDto = {
+      sender: 'user',
+      message: 'aaaaaaaaa'
+    }
+
     const res = await app.inject({
       method: 'POST',
       path: '/message-log',
-      payload: {
-        sender: 'user',
-        message: 'aaaaaaaaa'
-      }
+      payload
     });
 
     expect(res.statusCode).toEqual(201);
